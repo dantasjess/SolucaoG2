24c2548555713e512c0deb2183d06a40031eb9c4
dantasjess <jessica.dantas.rocha@gmail.com>
2021-08-16 16:24:17 -0300

    Adicionar arquivos de projeto.

---
+++ Serviço/Cadastros/FabricanteServico.cs
@@ -0,0 +1,36 @@
+﻿using Modelo.Cadastros;
+using Persistência.DAL.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serviço.Cadastros
+{
+    public class FabricanteServico
+    {
+        private FabricanteDAL fabricanteDAL = new FabricanteDAL();
+        public IQueryable<Fabricante> ObterFabricantesClassificadosPorNome()
+        {
+            return fabricanteDAL.ObterFabricantesClassificadosPorNome();
+        }
+        
+        public Fabricante ObterFabricantePorId(long id)
+        {
+            return fabricanteDAL.ObterFabricantePorId(id);
+        }
+
+        public void GravarFabricante(Fabricante fabricante)
+        {
+            fabricanteDAL.GravarFabricante(fabricante);
+        }
+        
+        public Fabricante EliminarFabricantePorId(long id)
+        {
+            Fabricante fabricante = fabricanteDAL.ObterFabricantePorId(id);
+            fabricanteDAL.EliminarFabricantePorId(id);
+            return fabricante;
+        }
+    }
+}
---
+++ Serviço/Tabelas/CategoriaServico.cs
@@ -0,0 +1,33 @@
+﻿using Modelo.Tabelas;
+using Persistência.DAL.Tabelas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serviço.Tabelas
+{
+    public class CategoriaServico
+    {
+        private CategoriaDAL categoriaDAL = new CategoriaDAL();
+        public IQueryable<Categoria> ObterCategoriasClassificadasPorNome()
+        {
+            return categoriaDAL.ObterCategoriasClassificadasPorNome();
+        }
+        public Categoria ObterCategoriaPorId(long id)
+        {
+            return categoriaDAL.ObterCategoriaPorId(id);
+        }
+        public void GravarCategoria(Categoria categoria)
+        {
+            categoriaDAL.GravarCategoria(categoria);
+        }
+        public Categoria EliminarCategoriaPorId(long id)
+        {
+            Categoria categoria = categoriaDAL.ObterCategoriaPorId(id);
+            categoriaDAL.EliminarCategoriaPorId(id);
+            return categoria;
+        }
+    }
+}
